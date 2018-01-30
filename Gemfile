de12328efff7c097656ca8316184c4f83a4061bb
Jon Norton <jmichaeln5@gmail.com>
2018-01-30 11:43:18 -0500

    added Figaro for security + configured mailer

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ git_source(:github) do |repo_name|
 end
 
 
+# use figaro for secrets
+gem "figaro"
+
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use postgresql as the database for Active Record
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,15 @@
 class UserMailer < ApplicationMailer
 
-
+  puts "*** UserMailer initialized ***"
+  
   #code goes here
 
   default from: 'hoorayfun123@gmail.com'
 
-  def welcome_email()
+  def welcome_email(user)
     p "*** welcome_email action fred ***"
+    @user = user
+    mail(to: @user.email, subject: "Welcome Brochachskis")
   end
 
 
